ace3974de162f8f9f3a89bdab835fa7749bdc7b3
jfrisks <jo1875fr-s@student.lu.se>
2019-09-14 18:18:34 +0200

    Added first small draft of userpage, without modal for changing info

--- src/views/UserPage.jsx
+++ src/views/UserPage.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import EmployeeCard from '../components/EmployeeCard'
+import { Fab, Grid, Container } from '@material-ui/core'
+
+import BuildIcon from '@material-ui/icons/Build'
 
 function UserPage(props) {
     return (
-        <div>
-            User
-        </div>
+        <Container>
+            <Grid
+                container
+                spacing={2}
+                justify="center"
+                alignItems="center"
+                alignContent="center"
+            >
+                <Grid item xs={12}>
+                    <EmployeeCard />
+                </Grid>
+                
+                <Grid item xl={6}>
+                    <Fab variant="extended" aria-label="delete">
+                        <BuildIcon />
+                        Change Password or Username
+                    </Fab>
+                </Grid>
+            </Grid> 
+        </Container>
     )
 }
 
